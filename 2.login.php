d72e9233eff57e44708688da6763e4054a597fe7
EriceHung <153830897+EriceHung@users.noreply.github.com>
2024-04-12 10:14:51 +0800

    Update and rename 0 to 2.login.php

---
+++ 2.login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8" /> <!-- 設置編碼為 UTF-8 -->
+        <title>登入範例</title> <!-- 設置頁面標題為 "登入範例" -->
+    </head>
+    <body>
+        <form method=post action="2.login.php">
+            帳號：<input type=text name="id"><br /> <!-- 輸入帳號 -->
+            密碼：<input type=password name="pwd"><p></p> <!-- 輸入密碼 -->
+            <input type=submit value="登入"> <input type=reset value="清除"> <!-- 登入和清除按鈕 -->
+        </form>
+    </body>
+</html>
