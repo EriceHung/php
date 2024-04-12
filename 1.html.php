95e0133e6c5c0eeed1d5ac4dc949e701a31d0eee
EriceHung <153830897+EriceHung@users.noreply.github.com>
2024-04-12 10:13:06 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <!-- 第一個 "Hello world!" -->
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <!-- 第二個 "Hello world!" -->
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <!-- 第三個 "Hello world!" -->
+    </body>
+</html>
